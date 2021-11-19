61fb2339d3e3ad7aab109ea726dd055cedddf7ad
Jacob Renny <82942568+JacobRenny@users.noreply.github.com>
2021-11-19 16:31:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+var noseX = 0;
+var noseY = 0;
+var rightWristX = 0;
+var leftWristX = 0;
+var difference = 0;
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPoses);
+}
+function modelLoaded() {
+    console.log("PoseNet is Initialized!");
+}
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX=" + noseX + "noseY=" + noseY);
+        rightWristX = results[0].pose.rightWrist.x;
+        leftWristX = results[0].pose.leftWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX=" + leftWristX + "rightWristX=" + rightWristX + "difference=" + difference);
+    }
+}
+function draw() {
+    background("#A7C7E7");
+    fill("#FFC0CB");
+    stroke("#FFC0CB");
+    square(noseX, noseY, difference);
+    document.getElementById("square_sides").innerHTML="Width and Height of this Square will be ="+ difference+"px";
+}
